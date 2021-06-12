ee3b0ebe556fb6efbc08a70c17738dd38922e4fb
Oguzhan Cevik <cevik.ogsan@gmail.com>
2021-06-12 13:19:39 +0300

    Constants

---
+++ src/main/java/io/github/oguzhancevik/stockmanagement/util/Constants.java
@@ -0,0 +1,12 @@
+package io.github.oguzhancevik.stockmanagement.util;
+
+public class Constants {
+
+    public static final class API {
+        public static final String BASE_MAPPING = "/api";
+        public static final String CATEGORY_MAPPING = BASE_MAPPING + "/categories";
+        public static final String SUB_CATEGORY_MAPPING = BASE_MAPPING + "/subcategories";
+        public static final String PRODUCT_MAPPING = BASE_MAPPING + "/products";
+    }
+
+}
