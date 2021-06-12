be568a1f4ec41a412bee569e305576db8e90ec2e
Oguzhan Cevik <cevik.ogsan@gmail.com>
2021-06-12 12:32:47 +0300

    Spring Boot Base Structure

---
+++ src/main/java/io/github/oguzhancevik/stockmanagement/StockManagementApplication.java
@@ -0,0 +1,13 @@
+package io.github.oguzhancevik.stockmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementApplication.class, args);
+	}
+
+}
