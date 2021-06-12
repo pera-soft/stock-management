3c9d2899751eab15f19c495aba8e44ebcbe7dcc1
Oguzhan Cevik <cevik.ogsan@gmail.com>
2021-06-13 01:48:30 +0300

    Swagger

--- src/main/java/io/github/oguzhancevik/stockmanagement/StockManagementApplication.java
+++ src/main/java/io/github/oguzhancevik/stockmanagement/StockManagementApplication.java
@@ -3,9 +3,11 @@ package io.github.oguzhancevik.stockmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class StockManagementApplication {
 
     public static void main(String[] args) {
